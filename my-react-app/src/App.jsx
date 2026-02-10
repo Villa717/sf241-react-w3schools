f4828a773464a52f6ffcb7e7d8b3238d4db6aaf6
Andrei Antonio G Villa <agvilla@student.apc.edu.ph>
2026-02-10 20:04:42 +0000

    Task 02 update your Vite-React website with sample code from W3Schools

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello SF241 this is WEBPROG lo siento </h1>
+      <h2> Welcome and have fun with React!  </h2>
+      <h3>Hosted on Vercel.com</h3>
+    </div>
+  );
+}
+
+export default App;
