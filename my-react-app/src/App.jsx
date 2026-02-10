f6e4b50e297847242a76c9d1ed3c4d5c37edc6fc
Andrei Antonio G Villa <agvilla@student.apc.edu.ph>
2026-02-11 04:14:03 +0800

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello SF241 this is WEBPROG lo siento </h1>
+      <h1>Hello SF241 this is Guns&Roses </h1>
       <h2> Welcome and have fun with React!  </h2>
       <h3>Hosted on Vercel.com</h3>
     </div>
